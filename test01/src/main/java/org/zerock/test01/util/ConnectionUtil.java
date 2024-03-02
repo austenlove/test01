bbd6b37f2f3e47b9ae70dd0f77cda6217e052fe0
PARKCY\Park <mangoperry2015@gmail.com>
2024-03-03 00:51:55 +0900

    [fix] mariaDB port 번호 변경(3307)

--- test01/src/main/java/org/zerock/test01/util/ConnectionUtil.java
+++ test01/src/main/java/org/zerock/test01/util/ConnectionUtil.java
@@ -14,7 +14,7 @@ public enum ConnectionUtil {
     ConnectionUtil() {
         HikariConfig config = new HikariConfig();
         config.setDriverClassName("org.mariadb.jdbc.Driver");
-        config.setJdbcUrl("jdbc:mariadb://localhost:3306/webdb");
+        config.setJdbcUrl("jdbc:mariadb://localhost:3307/webdb");
         config.setUsername("webuser");
         config.setPassword("webuser");
         config.addDataSourceProperty("cachePrepStmts", "true");
